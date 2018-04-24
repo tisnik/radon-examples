84b55499bad4e9906c6ff722f6f98fd6298e5a6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-24 14:49:59 +0200

    Code taken from hovnokod.cz

---
+++ v_poradi.py
@@ -0,0 +1,18 @@
+# taken from http://hovnokod.cz/2744
+
+def vporadi(x,y,z):
+    if (x<y) and (x<z):
+        if (y<z):
+            print(x,y,z)
+        else:
+            print(x,z,y)
+    if (y<x) and (y<z):
+        if (x<z):
+            print(y,x,z)
+        else:
+            print(y,z,x)
+    if (z<y) and (z<x):
+        if (y<x):
+            print(z,y,x)
+        else:
+            print(z,x,y)
