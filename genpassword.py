0d8a19b1c8db59166c26525a4e48e182d6f24d3e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-26 11:29:02 +0100

    Fixed issues #17 and #19

--- genpassword.py
+++ genpassword.py
@@ -35,14 +35,14 @@ def genpassword(wlc, maxchar, txt, List, verbose):
                                                                     word = wlc[i13] + wlc[i14] + wlc[i15]  # noqa: E501
 
                                                                 if int(maxchar) == 14:
-                                                                    word = wlc[i1] + wlc[i2]
-                                                                    + wlc[i3] + wlc[i4] \
-                                                                    + wlc[i5] + wlc[i6] \
-                                                                    + wlc[i7] + wlc[i8] \
-                                                                    + wlc[i9] + wlc[i10] \
-                                                                    + wlc[i11] + wlc[i12] \
-                                                                    + wlc[i13] + wlc[i14] \
-                                                                    + wlc[i15]
+                                                                    word = wlc[i1] + wlc[i2] \
+                                                                        + wlc[i3] + wlc[i4] \
+                                                                        + wlc[i5] + wlc[i6] \
+                                                                        + wlc[i7] + wlc[i8] \
+                                                                        + wlc[i9] + wlc[i10] \
+                                                                        + wlc[i11] + wlc[i12] \
+                                                                        + wlc[i13] + wlc[i14] \
+                                                                        + wlc[i15]
   
                                                                 if int(maxchar) == 15 :
                                                                     word = wlc[i1] + wlc[i2] + wlc[i3] + wlc[i4] \
