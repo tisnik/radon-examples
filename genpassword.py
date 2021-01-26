4f4136da27aeb4fa07b905efb2403414782ac859
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-26 10:43:40 +0100

    Fixed issue #5

--- genpassword.py
+++ genpassword.py
@@ -1,7 +1,7 @@
 # taken from http://hovnokod.cz/1429 and slightly updated
 
 
-def genpassword(wlc, maxchar, txt, List,verbose):
+def genpassword(wlc, maxchar, txt, List, verbose):
     word = ""
     i1 = i2 = i3 = i4 = i5 = i6 = i6 = i7 = i8 = i9 = i10 = i11 = i12 = i13 = i14 = i15 = 0
     txtfile = open(txt,'w')
