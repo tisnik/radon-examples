0d8fb7a81fb4f69f154de825084c29479fe153f8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-26 10:43:00 +0100

    Fixed issue #1

--- genpassword.py
+++ genpassword.py
@@ -1,5 +1,6 @@
 # taken from http://hovnokod.cz/1429 and slightly updated
 
+
 def genpassword(wlc,maxchar,txt,List,verbose):
     word = ""
     i1 = i2 = i3 = i4 = i5 = i6 = i6 = i7 = i8 = i9 = i10 = i11 = i12 = i13 = i14 = i15 = 0
