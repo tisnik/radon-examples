d67f054b8d14fe9773b550502f57f91a656e6a4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-28 12:18:41 +0100

    Fixed issue #53

--- genpassword.py
+++ genpassword.py
@@ -71,7 +71,7 @@ def genpassword(wlc, maxchar, txt, List, verbose):
                                             if end == 1: break
                                         if end == 1: break
                                     if end == 1: break
-                                if end == 1 : break
+                                if end == 1: break
                             if end == 1 : break
                         if end == 1 : break
                     if end == 1 : break
